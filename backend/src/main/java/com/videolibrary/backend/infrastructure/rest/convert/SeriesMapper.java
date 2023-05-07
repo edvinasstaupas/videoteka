4761f1d269934639810211d99c9ed0e9832c1dc2
edvinasstaupas <staupas.edvinas@gmail.com>
2023-05-07 19:28:07 +0300

    remove unused import

--- backend/src/main/java/com/videolibrary/backend/infrastructure/rest/convert/SeriesMapper.java
+++ backend/src/main/java/com/videolibrary/backend/infrastructure/rest/convert/SeriesMapper.java
@@ -3,7 +3,6 @@ package com.videolibrary.backend.infrastructure.rest.convert;
 import com.videolibrary.backend.domain.entity.Series;
 import com.videolibrary.backend.infrastructure.rest.dto.SeriesDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(config = DefaultMapperConfig.class)
 public interface SeriesMapper {
