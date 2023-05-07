74df50fc92452b6d10911286f7fc5fb733a31845
mantas.pilipuitis <mantas.pilipuitis@zenitech.co.uk>
2023-05-07 16:33:18 +0300

    VID-28 save and serve files

---
+++ backend/src/main/java/com/videolibrary/backend/property/StorageProperties.java
@@ -0,0 +1,22 @@
+package com.videolibrary.backend.property;
+
+import com.videolibrary.backend.dto.FileType;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.nio.file.Path;
+import java.util.Map;
+
+@Setter
+@ConfigurationProperties("storage")
+public class StorageProperties {
+    private Path rootStoragePath = Path.of("uploads");
+    private Map<FileType, String> storagePaths = Map.of(
+            FileType.VIDEO, "videos",
+            FileType.THUMBNAIL, "thumbnails"
+    );
+
+    public Path getStoragePath(FileType type) {
+        return rootStoragePath.resolve(storagePaths.get(type));
+    }
+}
---
+++ backend/src/main/java/com/videolibrary/backend/service/FileStorageService.java
@@ -0,0 +1,38 @@
+package com.videolibrary.backend.service;
+
+import com.videolibrary.backend.dto.FileType;
+import com.videolibrary.backend.property.StorageProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class FileStorageService {
+    private final StorageProperties properties;
+
+    public UUID store(MultipartFile file, FileType type) {
+        UUID fileId = UUID.randomUUID();
+        Path destinationPath = properties.getStoragePath(type).resolve(fileId.toString());
+        try {
+            Files.createDirectories(destinationPath.getParent());
+            file.transferTo(destinationPath);
+            return fileId;
+        } catch (IOException e) {
+            throw new UncheckedIOException("Failed to store file " + file.getOriginalFilename() + ". To " + destinationPath, e);
+        }
+    }
+
+    public Resource loadAsResource(String filename, FileType type) {
+        Path resourcePath = properties.getStoragePath(type).resolve(filename);
+        return UrlResource.from(resourcePath.toUri());
+    }
+}
