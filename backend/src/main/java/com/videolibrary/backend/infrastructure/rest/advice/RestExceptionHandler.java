07cc683cac3693a339ef642923d9c808c9f79824
Edvinas Staupas <73701414+edvinasstaupas@users.noreply.github.com>
2023-05-14 18:22:06 +0300

    add exception handling (#13)
    * add exception handling
    
    * change exception handling

---
+++ backend/src/main/java/com/videolibrary/backend/infrastructure/rest/advice/RestExceptionHandler.java
@@ -0,0 +1,86 @@
+package com.videolibrary.backend.infrastructure.rest.advice;
+
+import com.videolibrary.backend.domain.exception.DomainEntityNotFoundException;
+import com.videolibrary.backend.infrastructure.rest.dto.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@Slf4j
+@ControllerAdvice
+@Configuration("RestExceptionHandler")
+@RequiredArgsConstructor
+public class RestExceptionHandler {
+
+    private final HttpServletRequest request;
+
+    private ResponseEntity<ErrorResponse> toResponse(Exception exception, HttpStatus status) {
+        return toResponse(exception, status, exception.getMessage());
+    }
+
+    private ResponseEntity<ErrorResponse> toResponse(Exception exception, HttpStatus status, String message) {
+        ErrorResponse entity = new ErrorResponse();
+        entity.setException(exception.getClass().getSimpleName());
+        entity.setMessage(message);
+        return new ResponseEntity<>(entity, status);
+    }
+
+    // 400 bad request
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleInvalidObjects(Exception exception) {
+        var ex = (MethodArgumentNotValidException) exception;
+        var message = String.join("\n", ex.getBindingResult().getAllErrors().stream().map(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            return fieldName + ": " + errorMessage;
+        }).toList());
+        return toResponse(exception, HttpStatus.BAD_REQUEST, message);
+    }
+
+    // 400 bad request
+    @ExceptionHandler({ConstraintViolationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleConstraintViolation(Exception exception) {
+        var ex = (ConstraintViolationException) exception;
+        var message = String.join("\n", ex.getConstraintViolations().stream().map(ConstraintViolation::getMessageTemplate).toList());
+        return toResponse(exception, HttpStatus.BAD_REQUEST, message);
+    }
+
+    // 400 bad request
+    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class, HttpRequestMethodNotSupportedException.class, MissingServletRequestParameterException.class, MethodArgumentTypeMismatchException.class, HttpMessageNotReadableException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleBadRequest(Exception e) {
+        return toResponse(e, HttpStatus.BAD_REQUEST);
+    }
+
+    // 404 not found
+    @ExceptionHandler({NoHandlerFoundException.class, DomainEntityNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorResponse> handleNotFound(Exception e) {
+        return toResponse(e, HttpStatus.NOT_FOUND);
+    }
+
+    // 500 internal server error
+    @ExceptionHandler({Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ErrorResponse> handleDefaultExceptions(Exception e) {
+        return toResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
---
+++ backend/src/main/java/com/videolibrary/backend/infrastructure/rest/dto/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.videolibrary.backend.infrastructure.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String exception;
+    private String message;
+}
