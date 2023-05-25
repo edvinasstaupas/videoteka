4679fb0fa83d5157d2f8759c60926f59fa5b87c4
mantas.pilipuitis <mantas.pilipuitis@zenitech.co.uk>
2023-05-25 07:29:00 +0300

    VID-88 remove arbitrary string truncation

--- backend/src/main/java/com/videolibrary/backend/configuration/ExceptionCallbackConfiguration.java
+++ backend/src/main/java/com/videolibrary/backend/configuration/ExceptionCallbackConfiguration.java
@@ -4,15 +4,12 @@ import com.videolibrary.backend.domain.entity.Error;
 import com.videolibrary.backend.domain.service.UserService;
 import com.videolibrary.backend.infrastructure.rest.advice.ErrorResponseCallback;
 import com.videolibrary.backend.infrastructure.sql.repository.ErrorRepository;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.unit.DataSize;
 
 import java.time.Clock;
 import java.time.ZonedDateTime;
@@ -21,17 +18,11 @@ import java.time.ZonedDateTime;
 public class ExceptionCallbackConfiguration {
     private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionCallbackConfiguration.class);
 
-    private final DataSize traceableExceptionSize;
-
-    public ExceptionCallbackConfiguration(@Value("${traceable-exception-size}") DataSize traceableExceptionSize) {
-        this.traceableExceptionSize = traceableExceptionSize;
-    }
-
     @Bean
     @ConditionalOnProperty(value = "persist-exceptions", havingValue = "false", matchIfMissing = true)
     public ErrorResponseCallback logExceptionCallback() {
         return (exception, status) -> {
-            String stackTrace = extractStackTrace(exception);
+            String stackTrace = ExceptionUtils.getStackTrace(exception);
             LOGGER.error("Exception during processing. Status - {}. Exception - {}", status, stackTrace);
         };
     }
@@ -43,14 +34,10 @@ public class ExceptionCallbackConfiguration {
             Error error = new Error();
             error.setStatus(status.value());
             error.setTimestamp(ZonedDateTime.now(clock));
-            error.setStackTrace(extractStackTrace(exception));
+            error.setStackTrace(ExceptionUtils.getStackTrace(exception));
             error.setUser(userService.getUser());
             repository.save(error);
         };
     }
 
-    private String extractStackTrace(Exception exception) {
-        String originalStackTrace = ExceptionUtils.getStackTrace(exception);
-        return StringUtils.truncate(originalStackTrace, (int) traceableExceptionSize.toBytes());
-    }
 }
--- backend/src/main/java/com/videolibrary/backend/domain/entity/Error.java
+++ backend/src/main/java/com/videolibrary/backend/domain/entity/Error.java
@@ -1,5 +1,6 @@
 package com.videolibrary.backend.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +20,7 @@ public class Error {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(columnDefinition = "TEXT")
     private String stackTrace;
 
     private Integer status;
