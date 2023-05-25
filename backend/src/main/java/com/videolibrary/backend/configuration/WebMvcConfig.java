e983a3e237fb2dbc3cb3fc78aea997e279292834
Gustas Žilinskas <gustas.zilinskas@gmail.com>
2023-05-25 19:50:37 +0300

    Security Changes (#22)
    * Enable cors
    
    * enable bearer token resolution from URI
    
    * serve files as octet stream
    
    ---------
    
    Co-authored-by: mantas.pilipuitis <mantas.pilipuitis@zenitech.co.uk>

--- backend/src/main/java/com/videolibrary/backend/configuration/WebMvcConfig.java
+++ backend/src/main/java/com/videolibrary/backend/configuration/WebMvcConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
--- backend/src/main/java/com/videolibrary/backend/infrastructure/rest/controller/FileUploadController.java
+++ backend/src/main/java/com/videolibrary/backend/infrastructure/rest/controller/FileUploadController.java
@@ -4,15 +4,10 @@ import com.videolibrary.backend.infrastructure.service.FileResourceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
@@ -27,7 +22,7 @@ public class FileUploadController {
     public ResponseEntity<Resource> serveFile(@PathVariable UUID id) {
         Resource file = storageService.loadAsResource(id);
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE)
                 .body(file);
     }
 
