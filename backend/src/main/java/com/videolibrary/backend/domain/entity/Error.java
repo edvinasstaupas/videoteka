18652b5f3a52841707cef43ee63305c1b14f8d5a
mantas.pilipuitis <mantas.pilipuitis@zenitech.co.uk>
2023-05-25 07:29:00 +0300

    VID-88 remove arbitrary string truncation

--- backend/src/main/java/com/videolibrary/backend/domain/entity/Error.java
+++ backend/src/main/java/com/videolibrary/backend/domain/entity/Error.java
@@ -1,9 +1,11 @@
 package com.videolibrary.backend.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,6 +21,8 @@ public class Error {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Lob
+    @Column(columnDefinition = "TEXT")
     private String stackTrace;
 
     private Integer status;
