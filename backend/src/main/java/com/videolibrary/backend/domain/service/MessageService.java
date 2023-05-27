56238166c40701cf9f34676e7981c776b545f15f
gzili <gustas.zilinskas@gmail.com>
2023-05-27 15:51:50 +0300

    implement user role getter

--- backend/src/main/java/com/videolibrary/backend/domain/service/MessageService.java
+++ backend/src/main/java/com/videolibrary/backend/domain/service/MessageService.java
@@ -1,11 +1,15 @@
 package com.videolibrary.backend.domain.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MessageService {
+    private final AuthService authService;
+
     public void printAuthName() {
-        System.out.println(SecurityContextHolder.getContext().getAuthentication().getName());
+        System.out.println(authService.getCurrentUserId() + " (" + authService.getCurrentUserRole() + ")");
     }
 }
--- backend/src/main/java/com/videolibrary/backend/infrastructure/rest/advice/RequestInterceptor.java
+++ backend/src/main/java/com/videolibrary/backend/infrastructure/rest/advice/RequestInterceptor.java
@@ -1,29 +1,24 @@
 package com.videolibrary.backend.infrastructure.rest.advice;
 
+import com.videolibrary.backend.domain.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-
-import java.security.Principal;
-import java.util.Collection;
 
 @Component
 @RequiredArgsConstructor
 public class RequestInterceptor implements HandlerInterceptor {
     private static final Logger LOGGER = LoggerFactory.getLogger("AuditLog");
 
+    private final AuthService authService;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        Principal principal = request.getUserPrincipal();
-        Collection<GrantedAuthority> authorities = ((JwtAuthenticationToken) principal).getAuthorities();
-
-        LOGGER.info("User: {}, Authorities: {}, Called method: {}", principal.getName(), authorities, handler);
+        LOGGER.info("User: {}, Role: {}, Called method: {}", authService.getCurrentUserId(), authService.getCurrentUserRole(), handler);
         return true;
     }
 }
