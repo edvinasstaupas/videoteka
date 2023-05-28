4021c33b69a502c0443ca9627d21b6b6508f574c
Edvinas Staupas <73701414+edvinasstaupas@users.noreply.github.com>
2023-05-28 21:48:43 +0300

    Update build.sh

--- build/backend/build.sh
+++ build/backend/build.sh
@@ -3,6 +3,6 @@ set -e
 
 gradle -p ./backend assemble
 
-docker build -t psk/backend:latest -f ./build/backend/Dockerfile -o ./images .
+docker build -t psk/backend:latest -f ./build/backend/Dockerfile .
 
-docker save psk/backend:latest >  psk_backend_latest.tar
+docker save psk/backend:latest > ./images/psk_backend_latest.tar
