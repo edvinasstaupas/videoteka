f67659f57379ddb15a1aa56875d085bd417eaa55
gzili <gustas.zilinskas@gmail.com>
2023-05-31 19:59:29 +0300

    remove unused imports

--- backend/src/main/java/com/videolibrary/backend/configuration/WebMvcConfig.java
+++ backend/src/main/java/com/videolibrary/backend/configuration/WebMvcConfig.java
@@ -3,14 +3,10 @@ package com.videolibrary.backend.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @ConditionalOnProperty(value = "logging.interceptor.enabled", havingValue = "true", matchIfMissing = true)
