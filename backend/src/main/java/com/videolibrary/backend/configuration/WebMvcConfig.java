61dd2ea645a66a072d03a99c85024c1816403630
gzili <gustas.zilinskas@gmail.com>
2023-05-31 19:58:05 +0300

    allow all request methods in CORS

--- backend/src/main/java/com/videolibrary/backend/configuration/WebMvcConfig.java
+++ backend/src/main/java/com/videolibrary/backend/configuration/WebMvcConfig.java
@@ -3,10 +3,14 @@ package com.videolibrary.backend.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @ConditionalOnProperty(value = "logging.interceptor.enabled", havingValue = "true", matchIfMissing = true)
@@ -21,6 +25,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("*");
     }
 }
