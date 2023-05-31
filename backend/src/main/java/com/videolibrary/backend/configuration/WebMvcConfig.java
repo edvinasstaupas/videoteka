47691fb09a444110404b0e2fc6cabc8d12c1eb96
Gustas Žilinskas <gustas.zilinskas@gmail.com>
2023-05-31 20:03:04 +0300

    Fix issues with PATCH requests (#30)
    * replace PUT with PATCH in movie update
    
    * allow all request methods in CORS
    
    * remove unused imports

--- backend/src/main/java/com/videolibrary/backend/configuration/WebMvcConfig.java
+++ backend/src/main/java/com/videolibrary/backend/configuration/WebMvcConfig.java
@@ -21,6 +21,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("*");
     }
 }
